36ca6f29c87a4ab8aced8c57b43dd3159bddbc28
Samrudh Nelli <samrudhnelli@gmail.com>
2025-09-06 22:32:59 +0530

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development? || Rails.env.test?
+  require "debug/prelude"
+end
+
 module SihPortal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
